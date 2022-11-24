1e370487a80c90fbdd0401b7a37e37d5f3f79087
Dishant <agrawaldishant29@gmail.com>
2022-11-24 10:43:45 +0530

    removed logger_service.dart

--- lib/hms_models.dart
+++ lib/hms_models.dart
@@ -3,7 +3,6 @@ library hms_models;
 export 'models/patient_model.dart';
 export 'utils/date_presentation.dart';
 export 'utils/extensions.dart';
-export 'utils/logger_service.dart';
 export 'utils/my_http_overrides.dart';
 export 'utils/my_safe_state.dart';
 export 'utils/my_toast.dart';
