6e4b94032ab0a41d6f9b5094cf37bfca78c56746
Dishant <agrawaldishant29@gmail.com>
2022-11-24 10:44:52 +0530

    modified my_toast.dart

--- lib/utils/my_toast.dart
+++ lib/utils/my_toast.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class MyToast{
-
   static _showToast(BuildContext context, String msg, int duration, Color toastColor, Color textColor) {
     Widget toast = Container(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
@@ -28,28 +27,29 @@ class MyToast{
       toastDuration: Duration(seconds: duration),
       positionedToastBuilder: (context, child) {
         return Positioned(
-          child: child,
           bottom: 100.0,
           left: 0,
           right: 0,
+          child: child,
         );
       },
     );
   }
 
-  static void showError(String msg,BuildContext context) {
+  static void showError({required BuildContext context, required String msg}) {
     _showToast(context, msg, 2, Colors.red, Colors.white);
   }
 
-  static void showSuccess(String msg,BuildContext context) {
+  static void showSuccess({required BuildContext context, required String msg}) {
     _showToast(context, msg, 2, Colors.green, Colors.white);
   }
 
-  static void normalMsg(String msg, BuildContext context) {
+  static void normalMsg({required BuildContext context, required String msg}) {
     ThemeData themeData = Theme.of(context);
     _showToast(context, msg, 2, themeData.colorScheme.primary, Colors.white);
   }
-  static void greyMsg(String msg, BuildContext context) {
+
+  static void greyMsg({required BuildContext context, required String msg}) {
     ThemeData themeData = Theme.of(context);
     _showToast(context, msg, 2, themeData.colorScheme.onBackground, Colors.white);
   }
--- lib/utils/my_utils.dart
+++ lib/utils/my_utils.dart
@@ -2,12 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:uuid/uuid.dart';
 
+import 'my_toast.dart';
+
 
 class MyUtils {
   static Future<void> copyToClipboard(BuildContext? context, String string) async {
     if(string.isNotEmpty) {
       await Clipboard.setData(ClipboardData(text: string));
       if(context != null) {
+        MyToast.showSuccess(context: context, msg: "Copied");
         // Snakbar().show_info_snakbar(context, "Copied");
       }
     }
