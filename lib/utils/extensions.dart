b2202494f9dab89828cb781ad7e87d4d7281571a
Dishant <agrawaldishant29@gmail.com>
2022-11-24 10:45:13 +0530

    added some new extensions

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
+extension EmptyCheckOnStringNullable on String? {
+  bool get checkNotEmpty => (this ?? '').isNotEmpty;
+
+  bool get checkEmpty => (this ?? '').isEmpty;
+}
+
+extension EmptyCheckOnMapNullable on Map? {
+  bool get checkNotEmpty => (this ?? {}).isNotEmpty;
+
+  bool get checkEmpty => (this ?? {}).isEmpty;
+}
+
+extension EmptyCheckOnListNullable on List? {
+  bool get checkNotEmpty => (this ?? []).isNotEmpty;
+
+  bool get checkEmpty => (this ?? []).isEmpty;
+}
+
+extension FirstOrLastElementInList<T> on List<T>? {
+  T? get firstElement => checkNotEmpty ? this!.first : null;
+
+  T? get lastElement => checkNotEmpty ? this!.last : null;
+}
+
 extension HexColor on Color {
   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
   static Color fromHex(String hexString) {
