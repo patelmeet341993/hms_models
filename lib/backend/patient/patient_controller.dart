7957e92eb2b4f797528baac66f4da209a21216ed
Dishant <agrawaldishant29@gmail.com>
2022-12-01 12:36:18 +0530

    added patient and visit backend

---
+++ lib/backend/patient/patient_controller.dart
@@ -0,0 +1,3 @@
+class PatientController {
+
+}
---
+++ lib/backend/patient/patient_repository.dart
@@ -0,0 +1,27 @@
+import 'package:hms_models/hms_models.dart';
+
+class PatientRepository {
+  Future<PatientModel?> getPatientModelFromPatientId({required String patientId}) async {
+    MyPrint.printOnConsole("PatientRepository().getPatientModelFromPatientId() called");
+
+    PatientModel? patientModel;
+
+    try {
+      if(patientId.isEmpty) {
+        MyPrint.printOnConsole("Patient Id Cannot be empty");
+        return patientModel;
+      }
+
+      MyFirestoreDocumentSnapshot snapshot = await FirebaseNodes.patientDocumentReference(patientId: patientId).get();
+      if(snapshot.exists && snapshot.data().checkNotEmpty) {
+        patientModel = PatientModel.fromMap(snapshot.data()!);
+      }
+    }
+    catch(e, s) {
+      MyPrint.printOnConsole("Error in PatientRepository().getPatientModelFromPatientId():$e");
+      MyPrint.printOnConsole(s);
+    }
+
+    return patientModel;
+  }
+}
