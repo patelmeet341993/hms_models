85c5ef0d8a5326e25a62fd2b24fd3238c6258fe4
Dishant <agrawaldishant29@gmail.com>
2022-12-01 11:46:48 +0530

    added scanQRAndGetData method in MyUtils to scan QR Code

--- lib/utils/my_utils.dart
+++ lib/utils/my_utils.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:uuid/uuid.dart';
 
-import 'package:hms_models/hms_models.dart' show MyPrint, MyToast;
+import 'package:hms_models/hms_models.dart' show MyPrint, MyToast, ParsingHelper;
+
+import '../view/components/qr_scanner_dialog.dart';
 
 class MyUtils {
   static Future<void> copyToClipboard(BuildContext? context, String string) async {
@@ -47,6 +49,17 @@ class MyUtils {
       MyPrint.printOnConsole(s);
       return null;
     }
+  }
+
+  static Future<String> scanQRAndGetData({required BuildContext context}) async {
+    dynamic value = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const QRScannerDialog();
+      },
+    );
+
+    return ParsingHelper.parseStringMethod(value);
   }
 }
 
