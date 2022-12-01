961b36fb057ed55ac11b1bd88b8d8aef6cb4f6b5
Dishant <agrawaldishant29@gmail.com>
2022-12-01 11:46:03 +0530

    created QRScannerDialog to scan the QR Code

---
+++ lib/view/components/qr_scanner_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QRScannerDialog extends StatelessWidget {
+  const QRScannerDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        height: 500,
+        width: 500,
+        child: Stack(
+          children: [
+            MobileScanner(
+              allowDuplicates: false,
+              controller: MobileScannerController(facing: CameraFacing.front, torchEnabled: true),
+              onDetect: (Barcode barcode, MobileScannerArguments? args) {
+                if (barcode.rawValue == null) {
+                  debugPrint('Failed to scan Barcode');
+                }
+                else {
+                  final String code = barcode.rawValue!;
+                  debugPrint('Barcode found! $code');
+                  if(code.isNotEmpty) {
+                    Navigator.pop(context,code);
+                  }
+                }
+              },
+            ),
+            Lottie.asset('assets/lotties/scan.json',),
+          ],
+        ),
+      ),
+    );
+  }
+}
