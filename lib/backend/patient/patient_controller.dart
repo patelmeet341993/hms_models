c1e3d6298a95059fb6ffb3b64a87700dee187e20
Dishant <agrawaldishant29@gmail.com>
2022-12-01 12:39:16 +0530

    added method to get patient model from id in PatientController

--- lib/backend/patient/patient_controller.dart
+++ lib/backend/patient/patient_controller.dart
@@ -1,3 +1,14 @@
-class PatientController {
+import '../../models/patient/patient_model.dart';
+import 'patient_repository.dart';
 
+class PatientController {
+  late PatientRepository patientRepository;
+
+  PatientController({PatientRepository? repository}) {
+    patientRepository = repository ?? PatientRepository();
+  }
+
+  Future<PatientModel?> getPatientModelFromPatientId({required String patientId}) async {
+    return patientRepository.getPatientModelFromPatientId(patientId: patientId);
+  }
 }
