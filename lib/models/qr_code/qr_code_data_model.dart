c1d53ac3f51a9230f521252a40e38fa23de5eaaf
Dishant <agrawaldishant29@gmail.com>
2022-12-02 18:49:55 +0530

    added QRCodeDataModel to store QR Code Data

---
+++ lib/models/qr_code/qr_code_data_model.dart
@@ -0,0 +1,35 @@
+import 'package:hms_models/hms_models.dart';
+
+class QRCodeDataModel {
+  String id = "", type = "";
+
+  QRCodeDataModel({
+    this.id = "",
+    this.type = "",
+  });
+
+  QRCodeDataModel.fromEncodedString(String data) {
+    dynamic value = MyUtils.decodeJson(data);
+
+    Map<String, dynamic> map = ParsingHelper.parseMapMethod(value);
+
+    id = ParsingHelper.parseStringMethod(map['id']);
+    type = ParsingHelper.parseStringMethod(map['type']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id" : id,
+      "type" : type,
+    };
+  }
+
+  String toEncodedString() {
+    return MyUtils.encodeJson(toMap);
+  }
+
+  @override
+  String toString() {
+    return toEncodedString();
+  }
+}
