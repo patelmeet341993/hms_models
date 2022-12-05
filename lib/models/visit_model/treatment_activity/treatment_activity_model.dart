d3b6fe34621c68a2b24377d943af699b278e9bc9
Dishant <agrawaldishant29@gmail.com>
2022-12-05 16:15:47 +0530

    added updatedTime in TreatmentActivityModel

--- lib/models/visit_model/treatment_activity/treatment_activity_model.dart
+++ lib/models/visit_model/treatment_activity/treatment_activity_model.dart
@@ -3,11 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hms_models/hms_models.dart' show ParsingHelper, MyUtils;
 
 class TreatmentActivityModel {
-  Timestamp? createdTime;
+  Timestamp? createdTime, updatedTime;
   String treatmentActivityStatus = "";
 
   TreatmentActivityModel({
     this.createdTime,
+    this.updatedTime,
     this.treatmentActivityStatus = "",
   });
 
@@ -21,12 +22,14 @@ class TreatmentActivityModel {
 
   void _initializeFromMap(Map<String, dynamic> map) {
     createdTime = ParsingHelper.parseTimestampMethod(map['createdTime']);
+    updatedTime = ParsingHelper.parseTimestampMethod(map['updatedTime']);
     treatmentActivityStatus = ParsingHelper.parseStringMethod(map['treatmentActivityStatus']);
   }
 
   Map<String, dynamic> toMap({bool toJson = false}){
     return  <String, dynamic>{
       'createdTime' : createdTime,
+      'updatedTime' : updatedTime,
       'treatmentActivityStatus' : treatmentActivityStatus,
     };
   }
