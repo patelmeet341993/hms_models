5790d0fcdc010bd5331a88c38e1db99aa268d103
DELL <virendesaivd77@gmail.com>
2022-12-30 12:59:30 +0530

    scanner ui update..

--- lib/view/components/qr_scanner_dialog.dart
+++ lib/view/components/qr_scanner_dialog.dart
@@ -13,34 +13,31 @@ class QRScannerDialog extends StatelessWidget {
         width: 500,
         child: Stack(
           children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              child: MobileScanner(
-                allowDuplicates: false,
-                controller: MobileScannerController(facing: CameraFacing.front, torchEnabled: true),
-                onDetect: (Barcode barcode, MobileScannerArguments? args) {
-                  if (barcode.rawValue == null) {
-                    debugPrint('Failed to scan Barcode');
+            MobileScanner(
+              allowDuplicates: false,
+              controller: MobileScannerController(facing: CameraFacing.front, torchEnabled: true),
+              onDetect: (Barcode barcode, MobileScannerArguments? args) {
+                if (barcode.rawValue == null) {
+                  debugPrint('Failed to scan Barcode');
+                }
+                else {
+                  final String code = barcode.rawValue!;
+                  debugPrint('Barcode found! $code');
+                  if(code.isNotEmpty) {
+                    Navigator.pop(context,code);
                   }
-                  else {
-                    final String code = barcode.rawValue!;
-                    debugPrint('Barcode found! $code');
-                    if(code.isNotEmpty) {
-                      Navigator.pop(context,code);
-                    }
-                  }
-                },
-              ),
+                }
+              },
             ),
             Lottie.asset('assets/lotties/scan.json',),
             Positioned(
               top: 0,
-              right: 20,
+              right: 0,
               child: InkWell(
                   onTap: (){
                     Navigator.pop(context);
                   },
-                  child: Icon(Icons.close,color: Colors.black,size: 18,)),
+                  child: Icon(Icons.close,color: Colors.white,size: 18,)),
             ),
 
           ],
