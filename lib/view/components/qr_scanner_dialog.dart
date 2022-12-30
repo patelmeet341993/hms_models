6f971e88f71a2a53d5470d1bfb2e0c00a7741ba8
DELL <virendesaivd77@gmail.com>
2022-12-30 13:00:52 +0530

    scanner ui update..

--- lib/view/components/qr_scanner_dialog.dart
+++ lib/view/components/qr_scanner_dialog.dart
@@ -31,13 +31,13 @@ class QRScannerDialog extends StatelessWidget {
             ),
             Lottie.asset('assets/lotties/scan.json',),
             Positioned(
-              top: 0,
-              right: 0,
+              top: 10,
+              right: 10,
               child: InkWell(
                   onTap: (){
                     Navigator.pop(context);
                   },
-                  child: Icon(Icons.close,color: Colors.white,size: 18,)),
+                  child: Icon(Icons.close,color: Colors.white,size: 22,)),
             ),
 
           ],
