9342cc035218124719d81a711fa30ee60a9007b3
Dishant <agrawaldishant29@gmail.com>
2023-01-12 16:29:28 +0530

    added AppErrorCodes and AppErrorMessages classes to Keep Track of Error Codes and Messages in the app

---
+++ lib/configs/error_codes_and_messages.dart
@@ -0,0 +1,15 @@
+class AppErrorCodes {
+  static const String error = "Error";
+
+  static const String requiredInformationAreMissing = "REQUIRED-INFORMATION-ARE-MISSING";
+  static const String patientNotFound = "PATIENT-NOT-FOUND";
+  static const String alreadyHavingActiveVisitInSameHospitalCannotCreateNewVisit = "ALREADY-HAVING-ACTIVE-VISIT-FOR-SAME-HOSPITAL";
+}
+
+class AppErrorMessages {
+  static const String error = "Error Occurred";
+
+  static const String requiredInformationAreMissing = "Required information are missing";
+  static const String patientNotFound = "Patient Data Not Found";
+  static const String alreadyHavingActiveVisitInSameHospitalCannotCreateNewVisit = "Already Having Active Visit In Same Hospital. Cannot Create New Visit";
+}
