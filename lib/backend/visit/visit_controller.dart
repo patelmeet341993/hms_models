ace2f00992b8637477c15d1ac42669b9c58b92e4
Dishant <agrawaldishant29@gmail.com>
2023-01-12 16:34:07 +0530

    In VisitController,
    1) added getVisitModelFromVisitId method
    2) Added Validation For Active Visits of Patient in the same Hospital when creating new Visit
    3) Added Visit id in the Queue of Doctor when creating new Visit

--- lib/backend/visit/visit_controller.dart
+++ lib/backend/visit/visit_controller.dart
@@ -1,4 +1,7 @@
 import 'package:hms_models/backend/admin_user/admin_user_controller.dart';
+import 'package:hms_models/configs/error_codes_and_messages.dart';
+import 'package:hms_models/models/common/app_error_model.dart';
+import 'package:hms_models/models/common/data_response_model.dart';
 
 import '../../hms_models.dart';
 
@@ -9,19 +12,76 @@ class VisitController {
     visitRepository = repository ?? VisitRepository();
   }
 
-  Future<VisitModel?> createNewVisit({
-    String doctorId = "",
-    String doctorName = "",
-    String hospitalId = "",
-    required PatientModel patientModel,
+  Future<VisitModel?> getVisitModelFromVisitId({required String visitId}) async {
+    return visitRepository.getVisitModelFromVisitId(visitId: visitId);
+  }
+
+  Future<DataResponseModel<VisitModel>> createNewVisit({
+    required String doctorId,
+    required String doctorName,
+    required String hospitalId,
+    required String patientId,
     String description = "",
     double weight = 0,
+    bool waitForOtherOperations = false,
   }) async {
     MyPrint.printOnConsole("VisitController().createNewVisit() called");
 
+    //region Validate Required Parameters
+    if(doctorId.isEmpty || hospitalId.isEmpty || patientId.isEmpty) {
+      return DataResponseModel(
+        appErrorModel: AppErrorModel(
+          code: AppErrorCodes.requiredInformationAreMissing,
+          message: AppErrorMessages.requiredInformationAreMissing,
+        ),
+      );
+    }
+    //endregion
+
+    DataResponseModel<VisitModel> dataResponseModel;
+
     try {
+      //region Get PatientData and Validate
+      PatientModel? patientModel = await PatientController().getPatientModelFromPatientId(patientId: patientId);
+
+      if(patientModel == null) {
+        return DataResponseModel(
+          appErrorModel: AppErrorModel(
+            code: AppErrorCodes.patientNotFound,
+            message: AppErrorMessages.patientNotFound,
+          ),
+        );
+      }
+      //endregion
+
+      //region Check if Patient is Having any Active Visit For Same Hospital or Not
+      //If he is having, cannot create new visit
+      if(patientModel.activeVisits.isNotEmpty) {
+        Map<String, VisitModel> visits = await visitRepository.getVisitModelsFromVisitIds(visitIds: patientModel.activeVisits.keys.toList());
+
+        bool isHavingActiveVisitForSameHospital = false;
+
+        for(VisitModel visitModel in visits.values) {
+          if(visitModel.hospitalId == hospitalId) {
+            isHavingActiveVisitForSameHospital = true;
+            break;
+          }
+        }
+
+        if(isHavingActiveVisitForSameHospital) {
+          return DataResponseModel(
+            appErrorModel: AppErrorModel(
+              code: AppErrorCodes.alreadyHavingActiveVisitInSameHospitalCannotCreateNewVisit,
+              message: AppErrorMessages.alreadyHavingActiveVisitInSameHospitalCannotCreateNewVisit,
+            ),
+          );
+        }
+      }
+      //endregion
+
       NewDocumentDataModel newDocumentDataModel = await MyUtils.getNewDocIdAndTimeStamp();
 
+      //region Create New Visit Model by Filling Data
       VisitModel visitModel = VisitModel(
         id: newDocumentDataModel.docId,
         patientId: patientModel.id,
@@ -36,15 +96,7 @@ class VisitController {
         createdTime: newDocumentDataModel.timestamp,
         weight: weight,
         active: true,
-        patientModel: PatientModel(
-          id: patientModel.id,
-          name: patientModel.name,
-          gender: patientModel.gender,
-          bloodGroup: patientModel.bloodGroup,
-          dateOfBirth: patientModel.dateOfBirth,
-          userMobiles: [patientModel.primaryMobile],
-          totalVisits: patientModel.totalVisits,
-        ),
+        patientModel: patientModel,
         isTreatmentActiveStream: true,
         treatmentActivity: [
           TreatmentActivityModel(
@@ -59,19 +111,35 @@ class VisitController {
           ),
         ],
       );
+      //endregion
 
       await FirebaseNodes.visitDocumentReference(visitId: visitModel.id).set(visitModel.toMap(),);
 
-      PatientController().addVisitIdInActiveVisits(patientId: patientModel.id, visitId: visitModel.id);
-      AdminUserController().addVisitIdInPatientsQueue(doctorId: doctorId, visitId: visitModel.id);
+      //region Perform Other Operations Related to Visit Creation
+      List<Future> futures = [
+        PatientController().addVisitIdInActiveVisits(patientId: patientModel.id, visitId: visitModel.id),
+        AdminUserController().addVisitIdInPatientsQueue(doctorId: doctorId, visitId: visitModel.id),
+      ];
 
-      return visitModel;
+      if(waitForOtherOperations) await Future.wait(futures);
+      //endregion
+
+      dataResponseModel = DataResponseModel<VisitModel>(data: visitModel,);
     }
     catch(e, s) {
       MyPrint.printOnConsole("Error in VisitController().createNewVisit():$e");
       MyPrint.logOnConsole(s);
+
+      dataResponseModel = DataResponseModel<VisitModel>(
+        appErrorModel: AppErrorModel(
+          exception: e,
+          stackTrace: s,
+        ),
+      );
     }
 
-    return null;
+    return dataResponseModel;
   }
+
+
 }
