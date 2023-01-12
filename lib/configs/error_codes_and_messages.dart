9027a200dd262803043342b9e3bddc01729f442d
Dishant <agrawaldishant29@gmail.com>
2023-01-12 16:39:35 +0530

    added someErrorOccurredWhileCreatingVisit in AppErrorCodes and AppErrorMessages

--- lib/configs/error_codes_and_messages.dart
+++ lib/configs/error_codes_and_messages.dart
@@ -4,6 +4,7 @@ class AppErrorCodes {
   static const String requiredInformationAreMissing = "REQUIRED-INFORMATION-ARE-MISSING";
   static const String patientNotFound = "PATIENT-NOT-FOUND";
   static const String alreadyHavingActiveVisitInSameHospitalCannotCreateNewVisit = "ALREADY-HAVING-ACTIVE-VISIT-FOR-SAME-HOSPITAL";
+  static const String someErrorOccurredWhileCreatingVisit = "SOME-ERROR-OCCURRED-WHILE-CREATING-VISIT";
 }
 
 class AppErrorMessages {
@@ -12,4 +13,5 @@ class AppErrorMessages {
   static const String requiredInformationAreMissing = "Required information are missing";
   static const String patientNotFound = "Patient Data Not Found";
   static const String alreadyHavingActiveVisitInSameHospitalCannotCreateNewVisit = "Already Having Active Visit In Same Hospital. Cannot Create New Visit";
+  static const String someErrorOccurredWhileCreatingVisit = "Some Error Occurred While Creating Visit";
 }
